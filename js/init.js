419e86899d304ca1c3523f39a6d99d585fc8bd46
dandesap1001 <95158958+dandesap1001@users.noreply.github.com>
2021-11-27 23:58:27 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAV-kAqMEMS6ZHxF_D-TKfOlzLycr5LvYE",
+  authDomain: "dandesap1001-585e1.firebaseapp.com",
+  projectId: "dandesap1001-585e1",
+  storageBucket: "dandesap1001-585e1.appspot.com",
+  messagingSenderId: "518013482444",
+  appId: "1:518013482444:web:69e3eaface0878060dd969"
+});
